c5d565ed944da326a84d63105fc49d888a6ac8a9
Terminator-006 <siwangaurish003@gmail.com>
2024-06-27 21:16:05 +0530

    Add files via upload

--- MVP/photo/ph.js
+++ MVP/photo/ph.js
@@ -1,22 +1,4 @@
-// document.querySelectorAll('input[type="file"]').forEach(input => {
-//     input.addEventListener('change', (event) => {
-//       if (!event.target.files.length) {
-//         event.target.value = '';
-//       }
-//       checkInputs();
-//     });
-//   });
 
-//   function checkInputs() {
-//     const inputs = document.querySelectorAll('input[type="file"]');
-//     let allFilled = true;
-//     inputs.forEach(input => {
-//       if (!input.files.length) {
-//         allFilled = false;
-//       }
-//     });
-//     document.getElementById('next-button').disabled = !allFilled;
-//   }
 const nextButton = document.getElementById('next-button');
 document.addEventListener('DOMContentLoaded', function (){
     nextButton.addEventListener('click', function () {
